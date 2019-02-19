9ff386ddcd67505f1146ed42530c0c2810bee275
Oshima-Ryu <taylor233@sina.com>
2019-02-19 21:02:17 +0800

    add mybatis dependencies and mybatis generators

--- src/main/java/seckill/App.java
+++ src/main/java/seckill/App.java
@@ -1,21 +1,40 @@
 package seckill;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import seckill.dao.UserDOMapper;
+import seckill.dataobject.UserDO;
+
+import javax.annotation.Resource;
 
 /**
  * Hello world!
  *
  */
-@EnableAutoConfiguration
+//@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"seckill"})
 @RestController
+@MapperScan("seckill.dao")
 public class App 
 {
+//    @Resource
+    @Autowired
+    private UserDOMapper userDOMapper;
+
     @RequestMapping("/")
     public String home(){
-        return "Hello World!";
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if(userDO == null){
+            return "用户不存在";
+        }else{
+            return userDO.getName();
+        }
+//        return "Hello World!";
     }
     public static void main( String[] args ){
         System.out.println( "Hello World!" );
