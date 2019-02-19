f18a668a9ee375131660b798a64bf4e939b83a83
Oshima-Ryu <taylor233@sina.com>
2019-02-19 16:21:56 +0800

    the hello world of spring boot

--- src/main/java/seckill/App.java
+++ src/main/java/seckill/App.java
@@ -1,13 +1,24 @@
 package seckill;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  *
  */
+@EnableAutoConfiguration
+@RestController
 public class App 
 {
-    public static void main( String[] args )
-    {
+    @RequestMapping("/")
+    public String home(){
+        return "Hello World!";
+    }
+    public static void main( String[] args ){
         System.out.println( "Hello World!" );
+        SpringApplication.run(App.class,args);
     }
 }
