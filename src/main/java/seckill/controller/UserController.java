256384153ade2ee109b830aacfbbe39cae2f6510
Oshima-Ryu <taylor233@sina.com>
2019-02-21 17:09:32 +0800

    添加功能：用户注册的otpCode模块

--- src/main/java/seckill/controller/UserController.java
+++ src/main/java/seckill/controller/UserController.java
@@ -1,5 +1,6 @@
 package seckill.controller;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import seckill.service.UserModel;
 import seckill.service.impl.UserServiceImpl;
 
 import javax.annotation.Resource;
+import java.util.Random;
 
 
 @Controller("user")
@@ -19,6 +21,28 @@ public class UserController extends BaseController{
 
     @Resource
     private UserServiceImpl userService;
+
+    @Resource
+    private HttpServletRequest httpServletRequest;
+
+    @RequestMapping("/getotp")
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name="telphone")String telphone){
+        //生成otp验证码
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);
+        randomInt += randomInt;
+        String otpCode = String.valueOf(randomInt);
+
+        //将otp验证码与对应用户手机号关联
+        httpServletRequest.getSession().setAttribute(telphone, otpCode);
+
+
+        //将otp验证码通过短信发送给用户
+        System.out.println("telphone = " + telphone + " & otpCode = " + otpCode);
+
+        return CommonReturnType.create(null);
+    }
 
     @RequestMapping("/get")
     @ResponseBody
